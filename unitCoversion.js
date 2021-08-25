c4b314ca232bd17d6706f09ae0e86dde956bd708
Dhiraj Sharma <dhirajsharmads0@gmail.com>
2021-08-25 20:38:28 +0530

    added  takes User Inputs and does Unit Conversion of different Length units

---
+++ unitCoversion.js
@@ -0,0 +1,46 @@
+/**
+ * ****************************************************************************
+ * Purpose: The purpose of this program to convert the Feet to Inch, Meter to Feet and vice-versa.
+ *
+ * @author Dhiraj
+ * @version 1.0
+ * @since 24-08-2021
+ * ****************************************************************************
+ */
+var readLine = require('readline-sync');
+
+let result = 0;
+let val = 0;
+
+console.log("\n1. Feet to Inch \n2. Inch to Feet \n3. Feet to Meter \n4. Meter to Feet");
+
+let userInput = readLine.questionInt("\nSelect any option : ");
+let num = readLine.questionInt("\nEnter the value you want to convert : ");
+
+try{
+    switch (userInput) {
+        case 1:
+            val = 12;  // 1 feet = 12 inches
+            result = num * val;
+            break;
+        case 2:
+            val = 12;
+            result = num / val;
+            break;
+        case 3:
+            val = 3.28084;  // 1 meter = 3.28084 feet
+            result = num / val;
+            break;
+        case 4:
+            val = 3.28084;
+            result = num * val;
+            break;
+        default:
+            console.log("\nInvalid Selection");
+    }  
+}catch{
+    console.log("\nInvalid Selection");
+}
+
+
+console.log("\nResult is " + result);
