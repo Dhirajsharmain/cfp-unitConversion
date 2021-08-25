5c015f33cf8a5687de53c66e3155b95db05f3794
Dhiraj Sharma <dhirajsharmads0@gmail.com>
2021-08-25 21:14:33 +0530

    added: modify the code to throw the error

--- unitCoversion.js
+++ unitCoversion.js
@@ -18,30 +18,27 @@ console.log("\n1. Feet to Inch \n2. Inch to Feet \n3. Feet to Meter \n4. Meter t
 let userInput = readLine.questionInt("\nSelect any option : ");
 let num = readLine.questionInt("\nEnter the value you want to convert : ");
 
-try{
-    switch (userInput) {
-        case 1:
-            val = 12;  // 1 feet = 12 inches
-            result = num * val;
-            break;
-        case 2:
-            val = 12;
-            result = num / val;
-            break;
-        case 3:
-            val = 3.28084;  // 1 meter = 3.28084 feet
-            result = num / val;
-            break;
-        case 4:
-            val = 3.28084;
-            result = num * val;
-            break;
-        default:
-            console.log("\nInvalid Selection");
-    }  
-}catch{
-    console.log("\nError Invalid Selection");
+switch (userInput) {
+    case 1:
+        val = 12; // 1 feet = 12 inches
+        result = num * val;
+        break;
+    case 2:
+        val = 12;
+        result = num / val;
+        break;
+    case 3:
+        val = 3.28084; // 1 meter = 3.28084 feet
+        result = num / val;
+        break;
+    case 4:
+        val = 3.28084;
+        result = num * val;
+        break;
+    default:
+        throw("\nInvalid Selection");
 }
+
 
 
 console.log("\nResult is " + result);
